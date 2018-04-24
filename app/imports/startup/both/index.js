3abd2defffb5e7e6f3b18d2c9acb2792b56519bf
Seth Kinel <sethak@hawaii.edu>
2018-04-23 19:31:12 -1000

    Rough user

--- app/imports/startup/both/index.js
+++ app/imports/startup/both/index.js
@@ -1,2 +1,4 @@
 import '/imports/api/stuff';
-import '/imports/api/session';
+import './user.js';
+import './session.js';
+
--- app/imports/startup/server/index.js
+++ app/imports/startup/server/index.js
@@ -1,3 +1,5 @@
 import './accounts.js';
 import './stuff.js';
+import './user.js';
 import './session.js';
+
